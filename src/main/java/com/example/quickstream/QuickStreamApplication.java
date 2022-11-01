c191e2de0f6a73859076c828dd71edd171a3495c
Pádraig McCarthy <padraig.mc1999@gmail.com>
2022-11-01 12:09:34 +0000

    Initial commit - base spring initializr app

---
+++ src/main/java/com/example/quickstream/QuickStreamApplication.java
@@ -0,0 +1,13 @@
+package com.example.quickstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickStreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quickstream/QuickStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quickstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
