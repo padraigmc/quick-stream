e2dcd576fbec3ad3ba113afd273e0cc4886d01ee
Padraig McCarthy <padraig.mc1999@gmail.com>
2022-11-22 13:09:41 +0000

    Removed sql files from resources

--- backend/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-SELECT 'CREATE DATABASE quickstream'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'quickstream')\gexec
-
-CREATE TABLE IF NOT EXISTS videos (
-    id serial PRIMARY KEY,
-    name varchar(50) NOT NULL,
-    path varchar(50) NOT NULL
-);
