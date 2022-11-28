10d9728b71fd412f89894d537c2f9d7acac98b45
Padraig McCarthy <padraig.mc1999@gmail.com>
2022-11-28 15:33:06 +0000

    Added react-router to the front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,23 @@
-import UploadVideo from "./components/upload-video";
-import VideoList from "./components/video-list";
-import VideoPlayer from "./components/video-player";
+import { Switch, Route } from 'react-router-dom';
+import React from 'react';
+import './styles.css';
 
-function App() {
-  return (
-    <div className="App">
-        <UploadVideo />
-        <VideoList />
-        <VideoPlayer />
-    </div>
-  );
-}
+import NavBar from "./components/nav-bar";
+import Home from './pages/home/home';
+import Browse from './pages/browse/browse';
+import Upload from './pages/upload/upload';
+import Watch from './pages/watch/watch';
+
+const App = () => (
+  <div className="app">
+    <NavBar />
+    <Switch>
+      <Route exact path='/' component={Home}></Route>
+      <Route exact path='/browse' component={Browse}></Route>
+      <Route exact path='/upload' component={Upload}></Route>
+      <Route exact path='/watch/:id' component={Watch}></Route>
+    </Switch>
+  </div>
+);
 
 export default App;
